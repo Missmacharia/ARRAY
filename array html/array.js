8255c9e96d9ada948effa1a55b84e387ee63fa93
Missmacharia <yvonnemacharia30@gmail.com>
2022-10-08 22:20:42 +0300

    added a array.html

---
+++ array html/array.js
@@ -0,0 +1,401 @@
+// const details = [
+//     {
+//         image:"../images/image-daniel.jpg",
+//         "profile": "Leanne Graham",
+//         "website":  "hildegard.org",
+//         "companyName": "Romaguera"
+//     },
+//     {
+//         image:"../images/image-jeanette.jpg",
+//         "profile": "Ervin Howell",
+//         "website":  "nastasia.net",
+//         "companyName": "Deckow-Crist"
+//     },
+//     {
+//         image:"../images/image-jonathan.jpg",
+//         "profile": "Clementine Bauch",
+//         "website":  "ramiro.info",
+//         "companyName": "Romaguera-Jacobson"
+//     },
+//     {
+//         image:"../images/image-kira.jpg",
+//         "profile": "Patricia Lebsack",
+//         "website":  "kale.biz",
+//         "companyName": "Robel-Corkery"
+//     },
+//     {
+//         image:"../images/image-patrick.jpg",
+//         "profile": "Chelsey Dietrich",
+//         "website":  "demarco.info",
+//         "companyName": "Keebler LLC" 
+//     },
+//     {
+//         image:"../images/image-daniel.jpg",
+//         "profile": "Mrs. Dennis Schulist",
+//         "website":  "ola.org",
+//         "companyName": "Considine-Lockman" 
+//     },
+//     {
+//         image:"../images/image-jeanette.jpg",
+//         "profile": "Kurtis Weissnat",
+//         "website":  "elvis.io",
+//         "companyName": "Johns Group" 
+//     },
+//     {
+//         image:"../images/image-jonathan.jpg",
+//         "profile": "Nicholas Runolfsdottir V",
+//         "website":  "jacynthe.com",
+//         "companyName": "bernathy Group" 
+
+//     },
+//     {
+//         image:"../images/image-kira.jpg",
+//         "profile": "Glenna Reichert",
+//         "website":  "conrad.com",
+//         "companyName": "Yost and Sons" 
+//     },
+//     {
+//         image:"../images/image-patrick.jpg",
+//         "profile": "Clementina DuBuque",
+//         "website":  "ambrose.net",
+//         "companyName": "Hoeger LLC" 
+//     }
+// ]
+
+
+const details =[
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "address": {
+        "street": "Skiles Walks",
+        "suite": "Suite 351",
+        "city": "Roscoeview",
+        "zipcode": "33263",
+        "geo": {
+          "lat": "-31.8129",
+          "lng": "62.5342"
+        }
+      },
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
+      "company": {
+        "name": "Keebler LLC",
+        "catchPhrase": "User-centric fault-tolerant solution",
+        "bs": "revolutionize end-to-end systems"
+      }
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "address": {
+        "street": "Norberto Crossing",
+        "suite": "Apt. 950",
+        "city": "South Christy",
+        "zipcode": "23505-1337",
+        "geo": {
+          "lat": "-71.4197",
+          "lng": "71.7478"
+        }
+      },
+      "phone": "1-477-935-8478 x6430",
+      "website": "ola.org",
+      "company": {
+        "name": "Considine-Lockman",
+        "catchPhrase": "Synchronised bottom-line interface",
+        "bs": "e-enable innovative applications"
+      }
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "address": {
+        "street": "Rex Trail",
+        "suite": "Suite 280",
+        "city": "Howemouth",
+        "zipcode": "58804-1099",
+        "geo": {
+          "lat": "24.8918",
+          "lng": "21.8984"
+        }
+      },
+      "phone": "210.067.6132",
+      "website": "elvis.io",
+      "company": {
+        "name": "Johns Group",
+        "catchPhrase": "Configurable multimedia task-force",
+        "bs": "generate enterprise e-tailers"
+      }
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "address": {
+        "street": "Ellsworth Summit",
+        "suite": "Suite 729",
+        "city": "Aliyaview",
+        "zipcode": "45169",
+        "geo": {
+          "lat": "-14.3990",
+          "lng": "-120.7677"
+        }
+      },
+      "phone": "586.493.6943 x140",
+      "website": "jacynthe.com",
+      "company": {
+        "name": "Abernathy Group",
+        "catchPhrase": "Implemented secondary concept",
+        "bs": "e-enable extensible e-tailers"
+      }
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "address": {
+        "street": "Dayna Park",
+        "suite": "Suite 449",
+        "city": "Bartholomebury",
+        "zipcode": "76495-3109",
+        "geo": {
+          "lat": "24.6463",
+          "lng": "-168.8889"
+        }
+      },
+      "phone": "(775)976-6794 x41206",
+      "website": "conrad.com",
+      "company": {
+        "name": "Yost and Sons",
+        "catchPhrase": "Switchable contextually-based project",
+        "bs": "aggregate real-time technologies"
+      }
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "address": {
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "city": "Lebsackbury",
+        "zipcode": "31428-2261",
+        "geo": {
+          "lat": "-38.2386",
+          "lng": "57.2232"
+        }
+      },
+      "phone": "024-648-3804",
+      "website": "ambrose.net",
+      "company": {
+        "name": "Hoeger LLC",
+        "catchPhrase": "Centralized empowering task-force",
+        "bs": "target end-to-end models"
+      }
+    }
+  ]
+
+ 
+const detailscontainer = document.getElementById('company_details')
+
+details. forEach((company_details, index)=>{
+  const div= document.createElement('div')
+  const image= document.createElement('img')
+  image.src='https://liveboldandbloom.com/wp-content/uploads/2018/08/69cvZiwg-1-e1601321023201.jpeg'
+  const myId =document.createElement('p')
+  myId.textContent =company_details.id
+  const h1= document.createElement('h1')
+  h1.textContent= company_details.name
+  const h2= document.createElement('h2')
+  h2.textContent = company_details.username
+  const aEmail =document.createElement('a')
+  aEmail.href =company_details.email
+  aEmail.textContent= company_details.email
+
+  const div2= document.createElement('div')
+  const street= document.createElement('p')
+  street.textContent= company_details.address.street
+  const suite =document.createElement('p')
+  suite.textContent= company_details.address.suite
+  const city =document.createElement('p')
+  city.textContent =company_details.address.city
+  const zipcode= document.createElement('p')
+  zipcode.textContent =company_details.address.zipcode
+
+  div2.appendChild(street)
+  div2.appendChild(suite)
+  div2.appendChild(city)
+  div2.appendChild(zipcode)
+
+  const geo =document.createElement('div')
+  const lat =document.createElement('p')
+  lat.textContent= company_details.address.geo.lat
+  const lng =document.createElement('p')
+  lng.textContent= company_details.address.geo.lng
+
+  geo.appendChild(lat)
+  geo.appendChild(lng)
+
+  const div3 =document.createElement('div')
+  const phone =document.createElement('p')
+  phone.textContent= company_details.phone
+  const aweb =document.createElement('a')
+  aweb.href= company_details.website
+  aweb.textContent= company_details.website
+
+  div3.appendChild(phone)
+  div3.appendChild(aweb)
+
+  const company_tittle =document.createElement('div')
+  const companyName =document.createElement('h1')
+  companyName.textContent= company_details.company.name
+  const catchPhrase =document.createElement('h3')
+  catchPhrase.textContent= company_details.company.catchPhrase
+  const bs =document.createElement('p')
+  bs.textContent= company_details.company.bs
+
+  company_tittle.appendChild(companyName)
+  company_tittle.appendChild(catchPhrase)
+  company_tittle.appendChild(bs)
+
+  div.appendChild(image)
+  div.appendChild(myId)
+  div.appendChild(h1)
+  div.appendChild(h2)
+  div.appendChild(aEmail)
+  div.appendChild(div2)
+  div.appendChild(geo)
+  div.appendChild(div3)
+  div.appendChild(company_tittle)
+  div.className='info'
+  
+  
+  detailscontainer.appendChild(div)
+})
+
+console.log(details)
+
+
+//  const detailscontainer = document.getElementById('company_details')
+
+//  details.forEach((company_detail, index) =>{
+//     const div= document.createElement('div')
+//     const image= document.createElement('img')
+//     image.src='https://liveboldandbloom.com/wp-content/uploads/2018/08/69cvZiwg-1-e1601321023201.jpeg'
+//     const h1= document.createElement('h1')
+//     h1.textContent=company_detail.profile
+//     const p= document.createElement('p')
+//     p.textContent=company_detail.companyName
+//     const a= document.createElement('a')
+//     a.textContent=company_detail.website
+//     a.href=company_details.website
+//     div.appendChild(image)
+//     div.appendChild(h1)
+//     div.appendChild(p)
+//     div.appendChild(a)
+//     div.className='info'
+
+//     detailscontainer.appendChild(div)
+//  })
+//  console.log(details);
